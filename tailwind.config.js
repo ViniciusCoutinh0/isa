20e54b432b4340c801984a9965e47589dc94edac
Vinicius Coutinho <viniciuscoutinho@Viniciuss-iMac.local>
2023-12-05 22:47:21 -0300

    Init repository

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "sf-text": [
+        "SF Pro Text",
+        "SF Pro Icons",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+      "sf-display": [
+        "SF Pro Display",
+        "SF Pro Icons",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+    },
+  },
+  plugins: [],
+};
